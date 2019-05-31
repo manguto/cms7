22f5728c5bee19d8862b9191116eb18001fd0036
marcosagtorres <marcosagtorres@gmail.com>
2019-05-30 22:48:06 -0300

    Development...

---
+++ mvc/control/dev/ControlDevToolsModules.php
@@ -0,0 +1,34 @@
+<?php
+namespace manguto\cms5\mvc\control\dev;
+
+use manguto\cms5\mvc\view\dev\ViewDev;
+use manguto\cms5\mvc\model\DevToolsModules;
+
+   
+
+class ControlDevToolsModules extends ControlDevTools
+{
+
+    static function RunRouteAnalisys($app)
+    {        
+        $app->get('/dev/tools/modules', function () {
+            self::PrivativeDevZone();
+            ViewDev::load('tools_modules',get_defined_vars());
+        });        
+        
+        $app->post('/dev/tools/modules', function () {
+            self::PrivativeDevZone(); 
+            //deb($_POST);
+            {
+                $platform = $_POST['platform'];
+                $ModuleName = ucfirst($_POST['modulename']);
+            }
+            {
+                $results = DevToolsModules::GenerateFile($platform, $ModuleName);
+            }
+            ViewDev::load('tools_modules_result',get_defined_vars());
+        });
+    }    
+}
+
+?>
---
+++ mvc/model/DevToolsModules.php
@@ -0,0 +1,105 @@
+<?php
+namespace manguto\cms5\mvc\model;
+
+use manguto\cms5\lib\Arquivos;
+
+class DevToolsModules
+{
+    const menuFileEndBound = '{/if}';
+    static function GenerateFile($platform, $ModuleName)
+    {
+        // deb($platform,0); deb($modulename);        
+        {
+            $platform = strtolower($platform);
+            $Platform = ucfirst($platform);
+        }
+        {
+            $modulename = strtolower($ModuleName);
+        }
+        {
+            $fileListAndContent = self::getFileListAndContent($platform, $Platform);
+            // debc($fileListAndContent);
+        }
+        
+        {
+            $replacedFileListAndContent = self::replaceFileListAndContent($fileListAndContent, $modulename, $ModuleName);
+            //debc($replacedFileListAndContent);
+        }
+        {
+            $return = self::createFileList($replacedFileListAndContent);
+        }
+        {
+            $return[] = self::updateMenuLinks($platform,$ModuleName,$modulename); 
+        }
+        return $return;
+    }
+    
+    static function updateMenuLinks($platform,$ModuleName,$modulename){
+        $filename = "sis/tpl/$platform/_menu.html";
+        
+        $menufile_content = Arquivos::obterConteudo($filename);
+        
+        if($platform=='site'){
+            $link = "<a href='/$modulename' class='btn btn-sm btn-light mr-1'>$ModuleName</a>";
+        }else{
+            $link = "<a href='/$platform/$modulename' class='btn btn-sm btn-light mr-1'>$ModuleName</a>";
+        }
+        
+
+        
+        //verifica se o link já nao foi inserido
+        if(strpos($menufile_content, $link)===false){
+            //caso nao tenha sido inserido, insere-o
+            $replace = '    '.$link.chr(10).chr(10).self::menuFileEndBound;
+            $menufile_content = str_replace(self::menuFileEndBound, $replace, $menufile_content);
+            Arquivos::escreverConteudo($filename, $menufile_content);
+            return "Link '$ModuleName' inserido no menu do ".strtoupper($platform).".";
+        }else{
+            return "Link '$ModuleName' já presente no menu do ".strtoupper($platform).".";
+        }        
+    }
+
+    static function getFileListAndContent($platform, $Platform)
+    {
+        $return = [];
+        
+        $return["sis/control/$platform/Control{$Platform}Zzz.php"] = "";
+        $return["sis/view/$platform/View{$Platform}Zzz.php"] = "";
+        $return["sis/tpl/$platform/{$platform}_zzz.html"] = "";
+        
+        foreach (array_keys($return) as $filename) {
+            $content = Arquivos::obterConteudo($filename);
+            $return[$filename] = $content;
+        }
+        return $return;
+    }
+    static function replaceFileListAndContent($fileListAndContent, $modulename,$ModuleName)
+    {
+        $return = [];        
+        foreach ($fileListAndContent as $filename=>$content) {
+            {
+                $filename = str_replace('Zzz', $ModuleName, $filename);
+                $filename = str_replace('zzz', $modulename, $filename);
+            }
+            {
+                $content = str_replace('Zzz', $ModuleName, $content);
+                $content = str_replace('zzz', $modulename, $content);
+            }
+            $return[$filename] = $content;
+        }
+        return $return;
+    }
+    static function createFileList($replacedFileListAndContent)
+    {
+        $return = [];        
+        foreach ($replacedFileListAndContent as $filename=>$content) {
+            if(!file_exists($filename)){
+                Arquivos::escreverConteudo($filename, $content);
+                $return[] = "Arquivo '$filename' criado com sucesso!";
+            }else{
+                $return[] = "Arquivo '$filename' já existente. Nenhum procedimento a ser realizado.";
+            }            
+        }
+        return $return;
+    }
+}
