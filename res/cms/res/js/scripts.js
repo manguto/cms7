2b88d078736ce5cc6e8400c02645fdfd0eac64e5
marcosagtorres <marcosagtorres@gmail.com>
2020-03-10 17:34:04 -0300

    Popups stuff!

--- res/cms/res/js/scripts.js
+++ res/cms/res/js/scripts.js
@@ -136,11 +136,53 @@ function verify_overMascUnmasc_inputFields(){
 		input.attr('type', 'password');		
 	});
 }
-// ================================================================================================================================================
-// DOCUMENT READY
+//##########################################################################################
+//####################################### POPUP CONTROLE ###################################
+//##########################################################################################
+
+//2020-03-10 | obtem conteudo de ajuda e disponibiliza em um pop-up
+function popup(data){	
+	$('#popup .content').html(data);
+}
+
+//2020-03-10 | obtem conteudo de ajuda e disponibiliza em um pop-up
+function ajuda(identificador){
+	$.post('/transportes_onibus/ajuda/'+identificador,function(data){
+		popup(data);
+	});
+}
+//verifica possiveis links para ajuda em popup
+function ajudaPopupsVerificacao(){
+	$('a.ajuda').each(function(){
+		var a = $(this);
+		var href = a.attr('href');
+		var identificador = href.replace("#", "");		
+		var title = a.prop('title');
+		//changes
+		if(title==''){
+			a.attr('title','Clique para ver algumas instruções e/ou ajuda.');
+		}
+		a.attr('href','javascript:void(0);');
+		a.attr('data-toggle','modal');
+		a.attr('data-target','#popup');
+		a.attr('data-id',identificador);
+		a.click(function(){
+			ajuda(identificador);
+			log(identificador);
+		});
+	});
+}
+
+
+//================================================================================================================================================
+//DOCUMENT READY
 
 $(document).ready(function() {
 	
+	//=====================================
+	//verificacao de links popup de ajuda
+	ajudaPopupsVerificacao();
+	//=====================================
 });
 
 
